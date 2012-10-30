21f1cc8b5746236cc2503163c467df68ac1fa0d8
hama@yoidore.org <hama@yoidore.org>
2012-10-30 18:09:56 +0900

    And a sign-out link.

--- lib/omniauth.rb
+++ lib/omniauth.rb
@@ -9,6 +9,11 @@ module OmniAuth
         # Obtain this from here: https://dev.twitter.com/apps/3548274/show
         provider :twitter, 'ofa8iNrVBp2Wkm15fEMg', 'jv4Jm1XlMJWKPz9m88lqPruAeAk8vcYAuASQTBfwu8'
       end
+      get '/auth/logout' do
+        session[:id] = nil
+        session[:token] = nil
+        redirect '/'
+      end
       get '/auth/:name/callback' do
         auth = request.env['omniauth.auth']
         unless user = User.first(:provider => auth.provider, :uid => auth.uid)
